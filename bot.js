65b6da9d97f091e821d35878d3e087bc4a8afc51
salix5 <salix5@livemail.tw>
2023-01-19 22:15:54 +0800

    slash command test

--- bot.js
+++ bot.js
@@ -6,9 +6,10 @@ const setname = require('./data/setname.json');
 const ltable = require('./data/lflist.json');
 const ltable_md = require('./data/lflist_md.json');
 
-const fs = require('fs');
+const fs = require('node:fs');
+const path = require('node:path');
 const initSqlJs = require('sql.js');
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
 // type
@@ -543,11 +544,25 @@ initSqlJs().then(function(SQL){
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
 
+// get commnads in ./commands
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
+
 client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
-client.on(Events.MessageCreate, async (msg) => {
+client.on(Events.MessageCreate, async msg => {
 	let cmd = msg.content.substring(0, 3);
 	let search_string = msg.content.substring(3, INPUT_LIMIT);
 	if (cmd === 'q! ' || cmd === 'n! ') {
@@ -593,6 +608,24 @@ client.on(Events.MessageCreate, async (msg) => {
 		else {
 			await msg.channel.send('沒有符合搜尋的項目。');
 		}
+	}
+});
+
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand())
+		return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 	}
 });
 
