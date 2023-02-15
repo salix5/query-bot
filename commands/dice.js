474c4a5c222c23fa714c34c37a7d648cfe80cd0a
salix5 <salix5@livemail.tw>
2023-02-16 01:26:45 +0800

    add command: /dice

---
+++ commands/dice.js
@@ -0,0 +1,25 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { randomInt } = require('node:crypto');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('dice')
+		.setDescription('擲一個X面骰子。')
+		.addIntegerOption(option =>
+			option.setName('face')
+				.setDescription('面數')
+				.setRequired(true)
+				.setMinValue(2)
+				.setMaxValue(0xffffffffffff)
+	),
+	async execute(interaction) {
+		const face = interaction.options.getInteger('face');
+		if (face) {
+			const result = randomInt(1, face);
+			await interaction.reply(result.toString());
+		}
+		else {
+			await interaction.reply('Error');
+		}
+	},
+};
