6079d5df8947d1fc059b0408f143c567c61cb356
salix5 <salix5566@gmail.com>
2023-03-19 23:12:59 +0800

    commands: add db button

--- commands/card-jp.js
+++ commands/card-jp.js
@@ -1,5 +1,5 @@
 "use strict";
-const { SlashCommandBuilder } = require('discord.js');
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle, SlashCommandBuilder } = require('discord.js');
 const choice_table = require('../data/choices_jp.json');
 const ygo = require('../ygo-query.js');
 
@@ -31,10 +31,32 @@ module.exports = {
 		if (id) {
 			let result = [];
 			ygo.query_id(id, result);
-			if (result.length === 1)
-				await interaction.reply(ygo.print_data(result[0]));
-			else
+			if (result.length === 1) {
+				const card = result[0];
+				if (card.cid) {
+					const locale = (card.ot === 2) ? 'en' : 'ja';
+					const button1 = new ButtonBuilder()
+						.setStyle(ButtonStyle.Link)
+						.setLabel('DB')
+						.setURL(`https://www.db.yugioh-card.com/yugiohdb/card_search.action?ope=2&cid=${card.cid}&request_locale=${locale}`);
+					const row1 = new ActionRowBuilder()
+						.addComponents(button1);
+					if (locale === 'ja') {
+						const button2 = new ButtonBuilder()
+							.setStyle(ButtonStyle.Link)
+							.setLabel('Q&A')
+							.setURL(`https://www.db.yugioh-card.com/yugiohdb/faq_search.action?ope=4&cid=${card.cid}&request_locale=ja`);
+						row1.addComponents(button2);
+					}
+					await interaction.reply({ content: ygo.print_data(result[0]), components: [row1] });
+				}
+				else {
+					await interaction.reply(ygo.print_data(result[0]));
+				}
+			}
+			else {
 				await interaction.reply('沒有符合條件的卡片。');
+			}
 		}
 		else {
 			await interaction.reply('沒有符合條件的卡片。');
