359ad26275ceb92975dbfbaafedcb1809b76e0e8
salix5 <salix5566@gmail.com>
2023-06-09 16:40:33 +0800

    query: update TCG link

--- common_query.js
+++ common_query.js
@@ -12,17 +12,22 @@ module.exports = {
 			if (result.length === 1) {
 				const card = result[0];
 				if (card.cid) {
+					const row1 = new ActionRowBuilder();
 					const button1 = new ButtonBuilder()
 						.setStyle(ButtonStyle.Link)
-						.setLabel('DB')
 						.setURL(ygo.print_db_link(card.cid, card.ot));
-					const row1 = new ActionRowBuilder().addComponents(button1);
 					if (card.ot !== 2) {
+						button1.setLabel('DB');
+						row1.addComponents(button1);
 						const button2 = new ButtonBuilder()
 							.setStyle(ButtonStyle.Link)
 							.setLabel('Q&A')
 							.setURL(ygo.print_qa_link(card.cid));
 						row1.addComponents(button2);
+					}
+					else {
+						button1.setLabel('DB (TCG)');
+						row1.addComponents(button1);
 					}
 					await interaction.reply({ content: ygo.print_data(result[0]), components: [row1] });
 				}
