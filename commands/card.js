ab3aa8eb2611a92575d59e71f06cf5b80a86649b
salix5 <salix5566@gmail.com>
2023-09-17 01:53:01 +0800

    commands: minor update

--- commands/card.js
+++ commands/card.js
@@ -1,7 +1,7 @@
 "use strict";
 const { SlashCommandBuilder } = require('discord.js');
 const { autocomplete } = require('../common_all.js');
-const common2 = require('../common_query.js');
+const { query_command } = require('../common_query.js');
 const choice_table = require('../commands_data/choices_tc.json');
 const choices_tc_pre = require('../commands_data/choices_tc_pre.json');
 Object.assign(choice_table, choices_tc_pre);
@@ -21,6 +21,6 @@ module.exports = {
 		await autocomplete(interaction, choice_table);
 	},
 	async execute(interaction) {
-		await common2.query_command(interaction, choice_table);
+		await query_command(interaction, choice_table);
 	},
 };
