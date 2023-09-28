e7f72ead76208b11d99ae99aa4aedc23ffa716f7
salix5 <salix5566@gmail.com>
2023-09-29 01:26:44 +0800

    common_query: add create_reply()

--- commands/random.js
+++ commands/random.js
@@ -1,8 +1,9 @@
 "use strict";
 const { randomInt } = require('node:crypto');
 const { promisify } = require('node:util');
-const { ActionRowBuilder, ButtonBuilder, ButtonStyle, SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js');
 const ygo = require('../ygo-query.js');
+const { create_reply } = require('../common_query.js');
 const ocg_list = require('../data/ocg_list.json');
 const rand = promisify(randomInt);
 
@@ -14,18 +15,7 @@ module.exports = {
 		const id = ocg_list[await rand(ocg_list.length)];
 		const card = ygo.get_card(id);
 		if (card) {
-			const button1 = new ButtonBuilder()
-				.setStyle(ButtonStyle.Link)
-				.setLabel('DB')
-				.setURL(ygo.print_db_link(card.cid, card.ot));
-			const button2 = new ButtonBuilder()
-				.setStyle(ButtonStyle.Link)
-				.setLabel('Q&A')
-				.setURL(ygo.print_qa_link(card.cid));
-			const row1 = new ActionRowBuilder()
-				.addComponents(button1)
-				.addComponents(button2);
-			await interaction.reply({ content: ygo.print_card(card), components: [row1] });
+			await interaction.reply(create_reply(card));
 		}
 		else {
 			await interaction.reply('Error');
--- common_query.js
+++ common_query.js
@@ -2,44 +2,50 @@
 const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 const ygo = require('./ygo-query.js');
 
+function create_reply(card) {
+	if (card.cid) {
+		const row1 = new ActionRowBuilder();
+		const button1 = new ButtonBuilder()
+			.setStyle(ButtonStyle.Link)
+			.setURL(ygo.print_db_link(card.cid, card.ot));
+		if (card.ot !== 2) {
+			button1.setLabel('DB');
+			row1.addComponents(button1);
+			const button2 = new ButtonBuilder()
+				.setStyle(ButtonStyle.Link)
+				.setLabel('Q&A')
+				.setURL(ygo.print_qa_link(card.cid));
+			row1.addComponents(button2);
+		}
+		else {
+			button1.setLabel('DB (TCG)');
+			row1.addComponents(button1);
+		}
+		return { content: ygo.print_card(card), components: [row1] };
+	}
+	else if (card.cid === 0) {
+		const button1 = new ButtonBuilder()
+			.setStyle(ButtonStyle.Link)
+			.setLabel('Yugipedia')
+			.setURL(ygo.print_wiki_link(card.id));
+		const row1 = new ActionRowBuilder().addComponents(button1);
+		return { content: ygo.print_card(card), components: [row1] };
+	}
+	else {
+		return ygo.print_card(card);
+	}
+}
+
 module.exports = {
+	create_reply: create_reply,
+
 	async query_command(interaction, choice_table) {
 		const input = interaction.options.getString('input');
 		let id = choice_table[input];
 		if (id) {
 			const card = ygo.get_card(id);
 			if (card) {
-				if (card.cid) {
-					const row1 = new ActionRowBuilder();
-					const button1 = new ButtonBuilder()
-						.setStyle(ButtonStyle.Link)
-						.setURL(ygo.print_db_link(card.cid, card.ot));
-					if (card.ot !== 2) {
-						button1.setLabel('DB');
-						row1.addComponents(button1);
-						const button2 = new ButtonBuilder()
-							.setStyle(ButtonStyle.Link)
-							.setLabel('Q&A')
-							.setURL(ygo.print_qa_link(card.cid));
-						row1.addComponents(button2);
-					}
-					else {
-						button1.setLabel('DB (TCG)');
-						row1.addComponents(button1);
-					}
-					await interaction.reply({ content: ygo.print_card(card), components: [row1] });
-				}
-				else if (card.cid === 0) {
-					const button1 = new ButtonBuilder()
-						.setStyle(ButtonStyle.Link)
-						.setLabel('Yugipedia')
-						.setURL(ygo.print_wiki_link(card.id));
-					const row1 = new ActionRowBuilder().addComponents(button1);
-					await interaction.reply({ content: ygo.print_card(card), components: [row1] });
-				}
-				else {
-					await interaction.reply(ygo.print_card(card));
-				}
+				await interaction.reply(create_reply(card));
 			}
 			else {
 				await interaction.reply('沒有符合條件的卡片。');
