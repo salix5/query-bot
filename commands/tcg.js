7d4270fe0d80e612efcb409087530fdbb6603876
salix5 <salix5566@gmail.com>
2023-10-03 17:02:32 +0800

    commands: add test commands

---
+++ commands/tcg.js
@@ -0,0 +1,27 @@
+"use strict";
+const { SlashCommandBuilder } = require('discord.js');
+const { autocomplete } = require('../common_all.js');
+const { query_command } = require('../common_query.js');
+const choice_table = require('../commands_data/choices_en.json');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('tcg')
+		.setDescription('Find a card by name.')
+		.addStringOption(option =>
+			option.setName('input')
+				.setDescription('Card name')
+				.setRequired(true)
+				.setMaxLength(100)
+				.setAutocomplete(true)
+		),
+	is_test: true,
+	async autocomplete(interaction) {
+		await autocomplete(interaction, choice_table);
+	},
+	async execute(interaction) {
+		const input = interaction.options.getString('input');
+		const id = choice_table[input];
+		await query_command(interaction, id, 'en');
+	},
+};
--- deploy-commands.js
+++ deploy-commands.js
@@ -3,16 +3,21 @@ require('dotenv').config();
 const fs = require('node:fs');
 
 const clientId = process.env.CLIENT_ID;
-const guildId = 0;
+const guildId = process.env.GUILD_ID;
 const token = process.env.TOKEN;
 const commands = [];
+const test_commands = [];
+
 // Grab all the command files from the commands directory you created earlier
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
-	commands.push(command.data.toJSON());
+	if (command.is_test)
+		test_commands.push(command.data.toJSON());
+	else
+		commands.push(command.data.toJSON());
 }
 
 // Construct and prepare an instance of the REST module
@@ -33,6 +38,18 @@ const rest = new REST({ version: '10' }).setToken(token);
 		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
 	} catch (error) {
 		// And of course, make sure you catch and log any errors!
+		console.error(error);
+	}
+	try {
+		console.log(`Started refreshing ${test_commands.length} test commands.`);
+
+		const data = await rest.put(
+			Routes.applicationGuildCommands(clientId, guildId),
+			{ body: test_commands },
+		);
+
+		console.log(`Successfully reloaded ${data.length} test commands.`);
+	} catch (error) {
 		console.error(error);
 	}
 })();
