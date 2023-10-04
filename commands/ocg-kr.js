d111d0095ebabc79b965e4de0017c5f7e7fea9cf
salix5 <salix5566@gmail.com>
2023-10-05 02:11:54 +0800

    commands: add test command /ocg-kr

---
+++ commands/ocg-kr.js
@@ -0,0 +1,27 @@
+"use strict";
+const { SlashCommandBuilder } = require('discord.js');
+const { autocomplete } = require('../common_all.js');
+const { query_command } = require('../common_query.js');
+const choice_table = require('../commands_data/choices_kr.json');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('ocg-kr')
+		.setDescription('카드를 검색합니다.')
+		.addStringOption(option =>
+			option.setName('input')
+				.setDescription('카드명을')
+				.setRequired(true)
+				.setMaxLength(100)
+				.setAutocomplete(true)
+		),
+	is_test: true,
+	async autocomplete(interaction) {
+		await autocomplete(interaction, choice_table);
+	},
+	async execute(interaction) {
+		const input = interaction.options.getString('input');
+		const id = choice_table[input];
+		await query_command(interaction, id, 'ko');
+	},
+};
