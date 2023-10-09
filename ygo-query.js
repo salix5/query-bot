bcdfd27797c0fb338301c5c96b46b8513c9d53b3
salix5 <salix5566@gmail.com>
2023-10-09 17:02:56 +0800

    Update ygo-query.js

--- ygo-query.js
+++ ygo-query.js
@@ -128,13 +128,15 @@ const db_list = [];
 const domain = 'https://salix5.github.io';
 const fetch_db = fetch(`${domain}/CardEditor/cards.cdb`)
 	.then(response => response.arrayBuffer())
-	.then(buf => { file_list[0] = new Uint8Array(buf) });
+	.then(buf => new Uint8Array(buf));
 const fetch_db2 = fetch(`${domain}/cdb/pre-release.cdb`)
 	.then(response => response.arrayBuffer())
-	.then(buf => { file_list[1] = new Uint8Array(buf) });
+	.then(buf => new Uint8Array(buf));
 const db_ready = Promise.all([initSqlJs(), fetch_db, fetch_db2])
-	.then(([sql, _res1, _res2]) => {
+	.then(([sql, file1, file2]) => {
 		SQL = sql;
+		file_list.push(file1);
+		file_list.push(file2);
 		for (const file of file_list) {
 			db_list.push(new SQL.Database(file));
 		}
