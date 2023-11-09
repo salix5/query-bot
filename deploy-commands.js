2534cc3d8feb33ea2330f9653af0b236d8f557cf
salix5 <salix5566@gmail.com>
2023-11-09 12:58:44 +0800

    convert deploy to ES module

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,23 +1,27 @@
-const { REST, Routes } = require('discord.js');
-const fs = require('node:fs');
-//require('dotenv').config();
+import { REST, Routes } from 'discord.js';
+import { readdirSync } from 'node:fs';
 
 const clientId = process.env.CLIENT_ID;
 const guildId = process.env.GUILD_ID;
 const token = process.env.TOKEN;
-const commands = [];
+const public_commands = [];
 const test_commands = [];
 
-// Grab all the command files from the commands directory you created earlier
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandsURL = new URL('commands/', import.meta.url);
+const commandFiles = readdirSync(commandsURL).filter(file => file.endsWith('.js'));
+const import_list = [];
+for (const file of commandFiles) {
+	const fileURL = new URL(file, commandsURL);
+	import_list.push(import(fileURL));
+}
 
 // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
+const commands = await Promise.all(import_list);
+for (const command of commands) {
 	if (command.is_test)
 		test_commands.push(command.data.toJSON());
 	else
-		commands.push(command.data.toJSON());
+		public_commands.push(command.data.toJSON());
 }
 
 // Construct and prepare an instance of the REST module
@@ -26,13 +30,13 @@ const rest = new REST({ version: '10' }).setToken(token);
 // and deploy your commands!
 (async () => {
 	try {
-		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+		console.log(`Started refreshing ${public_commands.length} application (/) commands.`);
 
 		// The put method is used to fully refresh all commands in the guild with the current set
 		const data = await rest.put(
 			//Routes.applicationGuildCommands(clientId, guildId),
 			Routes.applicationCommands(clientId),
-			{ body: commands },
+			{ body: public_commands },
 		);
 
 		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
