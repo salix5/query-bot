2a6f71a7decf0517bc63bacbc959cfdcdd4173d7
salix5 <salix5566@gmail.com>
2023-11-11 21:18:50 +0800

    common_all: add choices_en, choices_jp, choices_kr

--- commands/ocg-kr.js
+++ commands/ocg-kr.js
@@ -1,7 +1,6 @@
 import { SlashCommandBuilder } from 'discord.js';
-import { autocomplete_default } from '../common_all.js';
+import { autocomplete_default, choices_kr as choice_table } from '../common_all.js';
 import { query_command } from '../common_query.js';
-import choice_table from '../commands_data/choices_kr.json' assert { type: 'json' };
 
 export const data = new SlashCommandBuilder()
 	.setName('ocg-kr')
