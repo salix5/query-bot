de453fd4d14b1bf9e8845ea7eaec536ab97a67d3
salix5 <salix5566@gmail.com>
2024-03-18 17:42:16 +0800

    add json-loader.mjs

--- ygo-query.mjs
+++ ygo-query.mjs
@@ -1,20 +1,20 @@
 import initSqlJs from 'sql.js';
-import ltable_ocg from './data/lflist.json' assert { type: 'json' };
-import ltable_tcg from './data/lflist_tcg.json' assert { type: 'json' };
-import ltable_md from './data/lflist_md.json' assert { type: 'json' };
+import { ltable_ocg } from './json-loader.mjs';
+import { ltable_tcg } from './json-loader.mjs';
+import { ltable_md } from './json-loader.mjs';
 
-import cid_entry from './data/cid_table.json' assert { type: 'json' };
-import name_table_en from './data/name_table_en.json' assert { type: 'json' };
-import name_table_jp from './data/name_table_jp.json' assert { type: 'json' };
-import name_table_kr from './data/name_table_kr.json' assert { type: 'json' };
-import md_name from './data/md_name.json' assert { type: 'json' };
-import md_name_en from './data/md_name_en.json' assert { type: 'json' };
-import md_name_jp from './data/md_name_jp.json' assert { type: 'json' };
+import { cid_entry } from './json-loader.mjs';
+import { name_table_en } from './json-loader.mjs';
+import { name_table_jp } from './json-loader.mjs';
+import { name_table_kr } from './json-loader.mjs';
+import { md_name } from './json-loader.mjs';
+import { md_name_en } from './json-loader.mjs';
+import { md_name_jp } from './json-loader.mjs';
 
-import lang_en from './lang/en.json' assert { type: 'json' };
-import lang_ja from './lang/ja.json' assert { type: 'json' };
-import lang_ko from './lang/ko.json' assert { type: 'json' };
-import lang_zhtw from './lang/zh-tw.json' assert { type: 'json' };
+import { lang_en } from './json-loader.mjs';
+import { lang_ja } from './json-loader.mjs';
+import { lang_ko } from './json-loader.mjs';
+import { lang_zhtw } from './json-loader.mjs';
 
 const domain = 'https://salix5.github.io/cdb';
 const fetch_db = fetch(`${domain}/cards.cdb`).then(response => response.arrayBuffer()).then(buf => new Uint8Array(buf));
