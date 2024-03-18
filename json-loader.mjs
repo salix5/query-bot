1f4d3079d35a39cce9a7505567b001968859e019
salix5 <salix5566@gmail.com>
2024-03-18 17:42:16 +0800

    add json-loader.mjs

---
+++ json-loader.mjs
@@ -0,0 +1,24 @@
+import ltable_ocg from './data/lflist.json' assert { type: 'json' };
+import ltable_tcg from './data/lflist_tcg.json' assert { type: 'json' };
+import ltable_md from './data/lflist_md.json' assert { type: 'json' };
+
+import cid_entry from './data/cid_table.json' assert { type: 'json' };
+import name_table_en from './data/name_table_en.json' assert { type: 'json' };
+import name_table_jp from './data/name_table_jp.json' assert { type: 'json' };
+import name_table_kr from './data/name_table_kr.json' assert { type: 'json' };
+import md_name from './data/md_name.json' assert { type: 'json' };
+import md_name_en from './data/md_name_en.json' assert { type: 'json' };
+import md_name_jp from './data/md_name_jp.json' assert { type: 'json' };
+
+import lang_en from './lang/en.json' assert { type: 'json' };
+import lang_ja from './lang/ja.json' assert { type: 'json' };
+import lang_ko from './lang/ko.json' assert { type: 'json' };
+import lang_zhtw from './lang/zh-tw.json' assert { type: 'json' };
+
+export {
+	ltable_ocg, ltable_tcg, ltable_md,
+	cid_entry,
+	name_table_en, name_table_jp, name_table_kr,
+	md_name, md_name_en, md_name_jp,
+	lang_en, lang_ja, lang_ko, lang_zhtw,
+}
