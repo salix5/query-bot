f04f9d3282959b13ed8cfe344ce8e5b870cd73fa
salix5 <salix5566@gmail.com>
2024-04-02 19:27:42 +0800

    commands: add /ocg-ae

--- commands/tcg-en.js
+++ commands/tcg-en.js
@@ -1,6 +1,7 @@
 import { SlashCommandBuilder } from 'discord.js';
 import { autocomplete_default } from '../common_all.js';
 import { query_command } from '../common_query.js';
+const request_locale = 'en';
 
 export const data = new SlashCommandBuilder()
 	.setName('tcg-en')
@@ -13,8 +14,8 @@ export const data = new SlashCommandBuilder()
 	);
 export const cooldown = 2;
 export async function autocomplete(interaction) {
-	await autocomplete_default(interaction, 'en');
+	await autocomplete_default(interaction, request_locale);
 }
 export async function execute(interaction) {
-	await query_command(interaction, 'en', 'en');
+	await query_command(interaction, request_locale, request_locale);
 }
--- ygo-json-loader.mjs
+++ ygo-json-loader.mjs
@@ -4,12 +4,14 @@ import ltable_md from './data/lflist_md.json' assert { type: 'json' };
 import md_card_list from './data/CardList.json' assert { type: 'json' };
 
 import cid_json from './data/cid_table.json' assert { type: 'json' };
+import ae_table from './data/name_table_ae.json' assert { type: 'json' };
 import en_table from './data/name_table_en.json' assert { type: 'json' };
 import jp_table from './data/name_table_jp.json' assert { type: 'json' };
 import kr_table from './data/name_table_kr.json' assert { type: 'json' };
 import md_en_table from './data/md_name_en.json' assert { type: 'json' };
 import md_jp_table from './data/md_name_jp.json' assert { type: 'json' };
 
+import lang_ae from './lang/ae.json' assert { type: 'json' };
 import lang_en from './lang/en.json' assert { type: 'json' };
 import lang_ja from './lang/ja.json' assert { type: 'json' };
 import lang_ko from './lang/ko.json' assert { type: 'json' };
@@ -23,6 +25,7 @@ function object_to_map(obj) {
 }
 
 const cid_table = object_to_map(cid_json);
+const name_table_ae = object_to_map(ae_table);
 const name_table_en = object_to_map(en_table);
 const name_table_jp = object_to_map(jp_table);
 const name_table_kr = object_to_map(kr_table);
@@ -36,34 +39,39 @@ for (const [cid, id] of cid_table) {
 	}
 }
 
+const official_name = Object.create(null);
+official_name['ae'] = 'ae_name';
+official_name['en'] = 'en_name';
+official_name['ja'] = 'jp_name';
+official_name['ko'] = 'kr_name';
+
 const lang = Object.create(null);
+lang['ae'] = lang_ae;
 lang['en'] = lang_en;
 lang['ja'] = lang_ja;
 lang['ko'] = lang_ko;
 lang['zh-tw'] = lang_zhtw;
 
 const collator_locale = Object.create(null);
+collator_locale['ae'] = 'en-US';
 collator_locale['en'] = 'en-US';
 collator_locale['ja'] = 'ja-JP';
 collator_locale['ko'] = 'ko-KR';
 collator_locale['zh-tw'] = 'zh-Hant';
 
 const bls_postfix = Object.create(null);
+bls_postfix['ae'] = ' (Normal)';
 bls_postfix['en'] = ' (Normal)';
 bls_postfix['ja'] = '（通常モンスター）';
 bls_postfix['ko'] = ' (일반)';
 bls_postfix['zh-tw'] = '（通常怪獸）';
-
-const official_name = Object.create(null);
-official_name['en'] = 'en_name';
-official_name['ja'] = 'jp_name';
-official_name['ko'] = 'kr_name';
 
 const game_name = Object.create(null);
 game_name['en'] = 'md_name_en';
 game_name['ja'] = 'md_name_jp';
 
 const name_table = Object.create(null);
+name_table['ae'] = name_table_ae;
 name_table['en'] = name_table_en;
 name_table['ja'] = name_table_jp;
 name_table['ko'] = name_table_kr;
