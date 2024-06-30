e379ca195dec42961657a9a9217f48d6010ceb3e
salix5 <salix5566@gmail.com>
2024-06-30 16:53:17 +0800

    bot: update delete msg

---
+++ commands/seventh.js
@@ -0,0 +1,22 @@
+import { SlashCommandBuilder } from 'discord.js';
+import { autocomplete_default } from '../common_all.js';
+import { query_command } from '../common_query.js';
+
+export const url = import.meta.url;
+export const data = new SlashCommandBuilder()
+	.setName('seventh')
+	.setDescription('以中文卡名搜尋卡片')
+	.addStringOption(option => option.setName('input')
+		.setDescription('卡名')
+		.setRequired(true)
+		.setMaxLength(50)
+		.setAutocomplete(true)
+	);
+
+data.contexts = [0, 1, 2];
+export async function autocomplete(interaction) {
+	await autocomplete_default(interaction, 'full');
+}
+export async function execute(interaction) {
+	await query_command(interaction, 'full', 'zh-tw');
+}
