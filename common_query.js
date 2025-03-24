19e7a789be03510686e8bc08cb8d721866b78710
salix5 <salix5566@gmail.com>
2025-03-25 02:29:32 +0800

    update fetch_desc

--- common_query.js
+++ common_query.js
@@ -26,7 +26,10 @@ async function fetch_desc(card, request_locale) {
 
 	const re_ptext = /<div class="frame pen_effect">.*?<div class="item_box_text">.*?([^\r\n\t]+).*?<\/div>/s;
 	const re_text = /<div class="text_title">.*?<\/div>.*?([^\r\n\t]+).*?<\/div>/s;
-	const raw_data = (await fetch(ygo.print_db_link(card.cid, request_locale))).text();
+	const resp = await fetch(ygo.print_db_link(card.cid, request_locale));
+	if (!resp.ok)
+		console.error('fetch_desc', card.cid, request_locale, resp.status);
+	const raw_data = await resp.text();
 	const res_text = re_text.exec(raw_data);
 	let ctext = '';
 	if (res_text) {
