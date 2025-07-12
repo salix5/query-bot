0073d8d3c0d7fae5f0e1afea63394ee05cd7ed98
salix5 <salix5566@gmail.com>
2025-07-12 23:56:01 +0800

    remove common-json-loader.js

--- common-json-loader.js
+++
@@ -1,9 +0,0 @@
-import tc_json from './commands_data/choices_tc.json' with { type: 'json' };
-import ruby_json from './commands_data/choices_ruby.json' with { type: 'json' };
-const tc_entries = Object.entries(tc_json);
-const ruby_entries = Object.entries(ruby_json);
-
-export {
-	tc_entries,
-	ruby_entries,
-}
