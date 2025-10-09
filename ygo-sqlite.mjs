889a910fbcb11d3360981d9fc14ff08d6da7a756
salix5 <salix5566@gmail.com>
2025-10-09 20:23:29 +0800

    add quotes to keyword

--- ygo-sqlite.mjs
+++ ygo-sqlite.mjs
@@ -23,7 +23,7 @@ export const ID_BLACK_LUSTER_SOLDIER = 5405695;
 export const ID_TYLER_THE_GREAT_WARRIOR = 68811206;
 export const ID_DECOY = 20240828;
 
-const column_names = "datas.id, datas.ot, datas.alias, datas.setcode, datas.type, datas.atk, datas.def, datas.level, datas.race, datas.attribute, texts.name, texts.desc";
+const column_names = `datas.id, datas.ot, datas.alias, datas.setcode, datas.type, datas.atk, datas.def, datas.level, datas.race, datas.attribute, texts.name, texts."desc"`;
 export const select_all = `SELECT ${column_names} FROM datas JOIN texts USING (id) WHERE 1`;
 export const select_id = `SELECT datas.id FROM datas JOIN texts USING (id) WHERE 1`;
 export const select_name = `SELECT datas.id, texts.name FROM datas JOIN texts USING (id) WHERE 1`;
