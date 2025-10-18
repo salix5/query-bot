6f89f5fc3e519398c7f57f1719443369d00565c6
salix5 <salix5566@gmail.com>
2025-10-19 02:25:52 +0800

    move export get_name to json-loader

--- ygo-query.mjs
+++ ygo-query.mjs
@@ -1,7 +1,7 @@
 import { writeFile } from 'node:fs/promises';
 import { ltable_ocg, ltable_tcg, ltable_md, pack_list, pre_release, genesys_point, complete_name_table } from './ygo-json-loader.mjs';
 import { lang, collator_locale, bls_postfix, official_name, game_name } from './ygo-json-loader.mjs';
-import { id_to_cid, cid_table, name_table, md_table, md_table_sc, md_card_list } from './ygo-json-loader.mjs';
+import { id_to_cid, cid_table, name_table, md_table, md_card_list } from './ygo-json-loader.mjs';
 import { escape_regexp, escape_wildcard, inverse_mapping, zh_collator, zh_compare } from './ygo-utility.mjs';
 import { db_url1, db_url2, fetch_db } from './ygo-fetch.mjs';
 import { card_types, monster_types, link_markers, md_rarity, spell_colors, trap_colors, CID_BLACK_LUSTER_SOLDIER, spell_types, trap_types, MAX_CARD_ID } from "./ygo-constant.mjs";
@@ -701,25 +701,6 @@ export function get_card(id) {
 		return null;
 	const card = card_table.get(id);
 	return card ?? null;
-}
-
-/**
- * Get the card name of `id` in the region `locale`.
- * @param {number} cid 
- * @param {string} locale 
- * @returns {string}
- */
-export function get_name(cid, locale) {
-	if (locale === 'md') {
-		return md_table_sc.get(cid) ?? '';
-	}
-	if (!complete_name_table[locale])
-		return '';
-	if (!complete_name_table[locale].has(cid))
-		return '';
-	if (cid === CID_BLACK_LUSTER_SOLDIER && complete_name_table[locale].has(4370))
-		return complete_name_table[locale].get(4370);
-	return complete_name_table[locale].get(cid);
 }
 
 /**
