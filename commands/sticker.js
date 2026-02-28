f69a2d8c520dcfaa2969e194cb150a0975700d2f
salix5 <salix5566@gmail.com>
2026-03-01 01:05:11 +0800

    add sticker command

---
+++ commands/sticker.js
@@ -0,0 +1,24 @@
+import { SlashCommandBuilder } from 'discord.js';
+
+export const module_url = import.meta.url;
+export const data = new SlashCommandBuilder()
+	.setName('sticker')
+	.setDescription('發送貼圖');
+data.integration_types = [0, 1];
+data.contexts = [0, 1, 2];
+const stickerId = '1051036645866098719';
+
+/**
+ * @param {import('discord.js').ChatInputCommandInteraction} interaction 
+ */
+export async function execute(interaction) {
+	await interaction.reply({ content: '已發送貼圖！', ephemeral: true });
+	try {
+		await interaction.channel.send({
+			stickers: [stickerId]
+		});
+	}
+	catch { 
+		console.error('Failed to send sticker in:', interaction.channel.name);
+	}
+}
