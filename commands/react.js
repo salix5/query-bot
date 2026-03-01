fd97cb4001f8c9ad46c72e71bdb9331135d09a64
salix5 <salix5566@gmail.com>
2026-03-01 14:23:28 +0800

    react: reply with emoji

--- commands/react.js
+++ commands/react.js
@@ -12,11 +12,5 @@ const emoji = '<:Hondoni:1068552863405592666>';
  * @param {import('discord.js').ChatInputCommandInteraction} interaction 
  */
 export async function execute(interaction) {
-	await interaction.reply({ content: 'Done.', ephemeral: true });
-	try {
-		await interaction.channel.send({ content: emoji });
-	}
-	catch { 
-		console.error('Failed to send the reaction in:', interaction.channel.name);
-	}
+	await interaction.reply(emoji);
 }
