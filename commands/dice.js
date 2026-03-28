ae3b9396df8bf4253de66b212fdf63e1e3c9cab0
salix5 <salix5566@gmail.com>
2026-03-28 23:58:08 +0800

    Replace randomInt with random_integer for dice roll

--- commands/dice.js
+++ commands/dice.js
@@ -1,7 +1,5 @@
 import { SlashCommandBuilder } from 'discord.js';
-import { randomInt } from 'node:crypto';
-import { promisify } from 'node:util';
-const rand = promisify(randomInt);
+import { random_integer } from '../ygo-utility.mjs';
 
 export const module_url = import.meta.url;
 export const data = new SlashCommandBuilder()
@@ -19,6 +17,6 @@ export async function execute(interaction) {
 	let face = interaction.options.getInteger('face');
 	if (!face)
 		face = 20;
-	const result = await rand(face) + 1;
+	const result = await random_integer(face) + 1;
 	await interaction.reply(`${result}`);
 }
