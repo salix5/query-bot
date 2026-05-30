87ebb332e9dfaaef09c76918c0206ee3a5f44e0c
salix5 <salix5566@gmail.com>
2026-05-31 00:15:01 +0800

    Fix /random

--- commands/random.js
+++ commands/random.js
@@ -2,7 +2,7 @@ import { SlashCommandBuilder } from 'discord.js';
 import { name_table, get_card, cid_table } from '../ygo-query.mjs';
 import { random_integer } from '../ygo-utility.mjs';
 import { create_reply } from '../common_query.js';
-const keys = Object.keys(name_table['ja']);
+const cid_list_ja = Object.keys(name_table['ja']).map(key => Number.parseInt(key, 10));
 
 export const module_url = import.meta.url;
 export const data = new SlashCommandBuilder()
@@ -11,7 +11,7 @@ export const data = new SlashCommandBuilder()
 data.integration_types = [0, 1];
 data.contexts = [0, 1, 2];
 export async function execute(interaction) {
-	const cid = keys[await random_integer(keys.length)];
+	const cid = cid_list_ja[await random_integer(cid_list_ja.length)];
 	const card = get_card(cid_table.get(cid));
 	if (card) {
 		await interaction.reply(create_reply(card, 'zh-tw'));
