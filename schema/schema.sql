37d2cb33f597e3a250caa28bfcbea8178b48ca3d
salix5 <salix5566@gmail.com>
2026-07-18 20:40:45 +0800

    Refactor schema update sql

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,6 +1,10 @@
 BEGIN TRANSACTION;
 ALTER TABLE datas ADD COLUMN rule_code INTEGER DEFAULT 0;
-UPDATE datas SET rule_code = alias, alias = 0 WHERE type & 0x4000 = 0 AND alias != 0 AND abs(id - alias) >= 20;
+UPDATE datas
+SET rule_code = alias, alias = 0
+WHERE alias != 0
+    AND abs(id - alias) >= 20
+    AND (type & 0x4000) = 0;
 UPDATE datas SET rule_code = alias, alias = 0 WHERE id = 5405695;
 UPDATE datas SET rule_code = 13331639 WHERE alias = 6218704;
 
@@ -13,16 +17,16 @@ UPDATE datas SET another_code = 10000070 WHERE id = 46232525;
 
 ALTER TABLE datas ADD COLUMN setcode1 TEXT DEFAULT '[]';
 UPDATE datas 
-SET setcode1 = CASE 
-    WHEN (setcode & 0xffff) = 0 
+SET setcode1 = CASE
+    WHEN setcode & 0xffff = 0
         THEN '[]'
-    WHEN ((setcode >> 16) & 0xffff) = 0 
+    WHEN ((setcode >> 16) & 0xffff) = 0
         THEN json_array(setcode & 0xffff)
-    WHEN ((setcode >> 32) & 0xffff) = 0 
+    WHEN ((setcode >> 32) & 0xffff) = 0
         THEN json_array(setcode & 0xffff, (setcode >> 16) & 0xffff)
-    WHEN ((setcode >> 48) & 0xffff) = 0 
+    WHEN ((setcode >> 48) & 0xffff) = 0
         THEN json_array(setcode & 0xffff, (setcode >> 16) & 0xffff, (setcode >> 32) & 0xffff)
-    ELSE 
+    ELSE
         json_array(setcode & 0xffff, (setcode >> 16) & 0xffff, (setcode >> 32) & 0xffff, (setcode >> 48) & 0xffff)
 END
 WHERE setcode != 0;
@@ -31,5 +35,5 @@ ALTER TABLE datas DROP COLUMN setcode;
 ALTER TABLE datas RENAME COLUMN setcode1 TO setcode;
 
 ALTER TABLE datas ADD COLUMN scale INTEGER DEFAULT 0;
-UPDATE datas SET scale = (level >> 24) & 0xff, level = level & 0xffff WHERE type & 0x1000000 != 0;
+UPDATE datas SET scale = (level >> 24) & 0xff, level = level & 0xffff WHERE (type & 0x1000000) != 0;
 COMMIT;
