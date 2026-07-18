3db1c20dec53a0a5e8965313d0fed01047192c69
salix5 <salix5566@gmail.com>
2026-07-18 18:44:15 +0800

    Update schema.sql

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 ALTER TABLE datas ADD COLUMN rule_code INTEGER DEFAULT 0;
-UPDATE datas SET rule_code = alias, alias = 0 WHERE (type & 0x4000) = 0 AND alias != 0 AND abs(id - alias) >= 20;
+UPDATE datas SET rule_code = alias, alias = 0 WHERE type & 0x4000 = 0 AND alias != 0 AND abs(id - alias) >= 20;
 UPDATE datas SET rule_code = alias, alias = 0 WHERE id = 5405695;
 UPDATE datas SET rule_code = 13331639 WHERE alias = 6218704;
 
@@ -31,5 +31,5 @@ ALTER TABLE datas DROP COLUMN setcode;
 ALTER TABLE datas RENAME COLUMN setcode1 TO setcode;
 
 ALTER TABLE datas ADD COLUMN scale INTEGER DEFAULT 0;
-UPDATE datas SET scale = (level >> 24) & 0xff, level = level & 0xffff WHERE (type & 0x1000000) != 0;
+UPDATE datas SET scale = (level >> 24) & 0xff, level = level & 0xffff WHERE type & 0x1000000 != 0;
 COMMIT;
