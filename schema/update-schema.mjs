3457f31ab661acf87400712bc626592c0ca277b8
salix5 <salix5566@gmail.com>
2026-07-18 21:19:05 +0800

    Update sql file name

--- schema/update-schema.mjs
+++ schema/update-schema.mjs
@@ -1,6 +1,6 @@
 import { readFile } from "node:fs/promises";
 
 export async function update_schema(db) {
-	const sql_schema = await readFile(new URL('./schema.sql', import.meta.url),'utf8');
+	const sql_schema = await readFile(new URL('./migrate.sql', import.meta.url),'utf8');
 	db.exec(sql_schema);
 }
