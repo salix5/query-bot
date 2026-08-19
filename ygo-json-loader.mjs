f0b1fc67de6a4e1f808acce90a29967d39128931
salix5 <salix5566@gmail.com>
2026-08-19 22:44:35 +0800

    Refactor object_to_map for improved readability

--- ygo-json-loader.mjs
+++ ygo-json-loader.mjs
@@ -21,16 +21,15 @@ import { CID_BLACK_LUSTER_SOLDIER } from './ygo-constant.mjs';
  * @returns {Map<number, any>}
  */
 function object_to_map(obj) {
-	const result = new Map();
-	for (const [prop, value] of Object.entries(obj)) {
-		const key = Number.parseInt(prop, 10);
-		if (!Number.isSafeInteger(key)) {
-			console.error('object_to_map: invalid key', prop);
-			continue;
-		}
-		result.set(key, value);
-	}
-	return result;
+	const entries = Object.entries(obj)
+		.map(([prop, value]) => {
+			const key = Number.parseInt(prop, 10);
+			if (!Number.isSafeInteger(key)) {
+				console.error('object_to_map: invalid key', prop);
+			}
+			return [key, value];
+		});
+	return new Map(entries);
 }
 
 /**
