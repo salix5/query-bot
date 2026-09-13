bdaa439d1dea320ead94053ca8bc715daa35ac0a
salix5 <salix5566@gmail.com>
2026-09-13 18:23:13 +0800

    Encapsulate properties in CardData object (#16)
    * Encapsulate properties in CardData object
    
    * Refactor print_data function to use CardData object

--- common_query.js
+++ common_query.js
@@ -59,7 +59,7 @@ export function create_reply(card, locale) {
 		msg.components.push(row_db);
 	}
 	else if (pack_name) {
-		const card_locale = (card.ot === 2) ? 'EN' : 'JP';
+		const card_locale = (card.data.ot === 2) ? 'EN' : 'JP';
 		const pack_index = (card.id % 1000).toString().padStart(3, '0');
 		const card_number = `${pack_name}-${card_locale}${pack_index}`;
 		const row1 = new ActionRowBuilder();
