35648e91b81912b61fdb80e94658e85ce933c0e6
salix5 <salix5566@gmail.com>
2026-09-15 10:34:30 +0800

    fetch: remove database URL constants

--- ygo-fetch.mjs
+++ ygo-fetch.mjs
@@ -1,9 +1,3 @@
-const domain = 'https://github.com/salix5/cdb/releases/latest/download';
-const db_url1 = `${domain}/cards.cdb`;
-const db_url2 = `${domain}/pre-release.cdb`;
-
-export { db_url1, db_url2 };
-
 /**
  * @param {string} url 
  * @returns 
